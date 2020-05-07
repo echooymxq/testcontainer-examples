d9ee3da06f649738eea1a7c25ab36beb9c43011f
echooymxq <echooy.mxq@gmail.com>
2020-05-07 23:38:25 +0800

    Add redis sample.

---
+++ redis/src/main/java/org/easywheelsoft/testcontainer/redis/TestContainerExamplesApplication.java
@@ -0,0 +1,16 @@
+package org.easywheelsoft.testcontainer.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:echooy.mxq@gmail.com">echooymxq</a>
+ */
+@SpringBootApplication
+public class TestContainerExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainerExamplesApplication.class, args);
+	}
+
+}
